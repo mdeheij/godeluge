9bf2ded118109f3d8641fc33535d4e771ca9c436
Mike de Heij <mdeheij@hostnet.nl>
2016-10-27 13:34:20 +0200

    Remove dead code

--- types.go
+++ types.go
@@ -35,32 +35,6 @@ type Error struct {
 	Code    int32
 }
 
-// type oldTorrentStatus struct {
-// 	Name                string
-// 	Progress            float64
-// 	ETA                 float64
-// 	Queue               interface{}
-// 	TotalSize           interface{} `json: "total_size"`
-// 	State               interface{}
-// 	NumberSeeds         interface{} `json: "num_seeds"`
-// 	TotalSeeds          interface{} `json: "total_seeds"`
-// 	NumPeers            interface{} `json: "num_peers"`
-// 	TotalPeers          interface{} `json: "total_peers"`
-// 	DownloadPayloadRate interface{} `json: "download_payload_rate"`
-// 	UploadPayloadRate   interface{} `json: "upload_payload_rate"`
-// 	Ratio               interface{}
-// 	DistributedCopies   interface{} `json: "distributed_copies"`
-// 	IsAutoManaged       interface{} `json: "is_auto_managed"`
-// 	TimeAdded           interface{} `json: "time_added"`
-// 	TrackerHost         interface{} `json: "tracker_host"`
-// 	SavePath            interface{} `json: "save_path"`
-// 	TotalDone           interface{} `json: "total_done"`
-// 	TotalUploaded       interface{} `json: "total_uploaded"`
-// 	MaxDownloadSpeed    interface{} `json: "max_download_speed"`
-// 	MaxUploadSpeed      interface{} `json: "max_upload_speed"`
-// 	SeedsPeerRatio      interface{} `json: "seeds_peers_ratio"`
-// }
-
 //TorrentStatus is the response of GetTorrentStatus and contains information about the requested torrent
 type TorrentStatus struct {
 	Name                string  `json:"name"`
